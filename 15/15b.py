f2d8b84e9d995e94c5813363934674c30ca8f6a7
Jens Christian Hillerup <jens@hillerup.net>
2020-12-16 23:50:31 +0100

    improvement?

--- 15/15b.py
+++ 15/15b.py
@@ -1,49 +1,45 @@
 import tqdm
-turns = list(map(int, open('input-little.txt').read().split(",")))
 
-def find_last_occurence(list, needle):
-    for j in range(len(list)-2, -1, -1):
-        if list[j] == needle:
-            return j
-    return None
+def set_occurence(occurences, key, value):
+    if occurences[key] is None:
+        occurences[key] = [value]
+        return
+    
+    occurences[key] = [occurences[key][-1], value]
 
-def run_game(turns):
-    occurences = dict()    
-    for i, v in enumerate(turns):
-        if occurences.get(v) is None:
-            occurences[v] = [i]
-        else:
-            occurences[v].append(i)
-
-    for i in tqdm.tqdm(range(len(turns), 30000000)):        
-        try:
-            lo2 = occurences[turns[i-1]][-2]
-        except:
-            lo2 = None
+def get_second_to_last_occurence(occurences, v):
+    try:
+        return occurences[v][-2]
+    except:
+        return None
         
+
+def run_game(seed, num_turns=2020):
+    occurences = [None]*num_turns
+    for i, v in enumerate(seed):
+        set_occurence(occurences, v, i)
+
+    seed_len = len(seed)
+    seed.extend([None]*(num_turns-len(seed)))
+
+    for i in tqdm.tqdm(range(seed_len, num_turns)):        
+        lo2 = get_second_to_last_occurence(occurences, seed[i-1])
         if lo2 is None:
-            turns.append(0)
-            try:
-                occurences[0].append(i)
-            except:
-                occurences[0] = [i]
+            last_seen_difference = 0
         else:
-            last_seen_turn = i-(lo2+1)
-            
-            turns.append(last_seen_turn)
-            try:
-                occurences[last_seen_turn].append(i)
-            except KeyError:
-                occurences[last_seen_turn] = [i]
+            last_seen_difference = i-(lo2+1)
 
-    return turns[-1]
+        set_occurence(occurences, last_seen_difference, i)
+        seed[i] = last_seen_difference
 
-#assert(run_game([0,3,6]) == 436)
-#assert(run_game([1,3,2]) == 1)
-#assert(run_game([2,1,3]) == 10)
-#assert(run_game([1,2,3]) == 27)
-#assert(run_game([2,3,1]) == 78)
-#assert(run_game([3,2,1]) == 438)
-#assert(run_game([3,1,2]) == 1836)
+    return seed[-1]
 
-print(run_game([7,12,1,0,16,2]))
+assert(run_game([0,3,6]) == 436)
+assert(run_game([1,3,2]) == 1)
+assert(run_game([2,1,3]) == 10)
+assert(run_game([1,2,3]) == 27)
+assert(run_game([2,3,1]) == 78)
+assert(run_game([3,2,1]) == 438)
+assert(run_game([3,1,2]) == 1836)
+
+print(run_game([7,12,1,0,16,2], 30000000))
