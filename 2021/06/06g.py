5f58b6a0b37f655a00f5499c2eaa3902be61bc90
Jens Christian Hillerup <jens@hillerup.net>
2021-12-06 13:42:20 +0100

    better golf

--- 2021/06/06g.py
+++ 2021/06/06g.py
@@ -1,8 +1,7 @@
 G,F = map(int,open('i').read().split(",")),[0]*9
 for f in G: F[f]+=1
-def G(v):
-    v.append(v.pop(0))
-    v[6] += v[8]
 for s in [80,176]:
-    for z in range(s):G(F)
+    for z in range(s):
+        F.append(F.pop(0))
+        F[6] += F[8]
     print(sum(F))
