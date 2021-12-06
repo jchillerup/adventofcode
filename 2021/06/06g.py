90763b78eef6863ebbe720af5550a8b785fbd1a9
Jens Christian Hillerup <jens@hillerup.net>
2021-12-06 13:49:07 +0100

    155 bytes

--- 2021/06/06g.py
+++ 2021/06/06g.py
@@ -1,6 +1,6 @@
 G,F=map(int,open('i').read().split(",")),[0]*9
 for f in G:F[f]+=1
 for z in range(256):
-    F.append(F.pop(0))
+    F=F[1:]+[F[0]]
     F[6]+=F[8]
     if z in [79,255]:print(sum(F))
