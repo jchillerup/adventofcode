79cd772b42aef589729642d662321aae0dcb698c
Jens Christian Hillerup <jens@hillerup.net>
2021-12-06 12:21:05 +0100

    better golf

--- 2021/06/06g.py
+++ 2021/06/06g.py
@@ -1,14 +1,10 @@
-from collections import defaultdict as D
-G,F,R = map(int,open('i').read().split(",")),D(int),range
+G,F,R = map(int,open('i').read().split(",")),[0]*9,range
 for f in G: F[f]+=1
 
-def L(v):
-    x = v[0]
-    for i in R(9): 
-        v[i]=v[(i+1)]
-    v[6]+=x
-    v[8]=x
+def G(v):
+    v.append(v.pop(0))
+    v[6] += v[8]
 
 for s in [80,176]:
-    for z in range(s):L(F)
-    print(sum(F.values()))
+    for z in range(s):G(F)
+    print(sum(F))
