3b9c60a7fa488a2d0e97ab27872bcd7907fe5550
Jens Christian Hillerup <jens@hillerup.net>
2021-12-07 11:35:51 +0100

    168

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -1,5 +1,3 @@
-def cost(n):return int(n*(1+n)/2)
-def mean(N):return sum(N)//len(N)
+def c(n):return n**2+n
 N=sorted(map(int,open('input.txt').read().split(",")))
-print(sum([abs(n-N[len(N)//2]) for n in N]))
-print(sum([cost(abs(n-mean(N))) for n in N]))
+print(sum([abs(n-N[len(N)//2]) for n in N]),sum([c(abs(n-sum(N)//len(N))) for n in N])//2)
