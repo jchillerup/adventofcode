cc0d49a0b477ac6c7f8a07fcbe216bc064b2d9e4
Jens Christian Hillerup <jens@hillerup.net>
2021-12-07 11:37:21 +0100

    160

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -1,3 +1,3 @@
 def c(n):return n**2+n
-N=sorted(map(int,open('input.txt').read().split(",")))
+N=sorted(map(int,open('i').read().split(",")))
 print(sum([abs(n-N[len(N)//2]) for n in N]),sum([c(abs(n-sum(N)//len(N))) for n in N])//2)
