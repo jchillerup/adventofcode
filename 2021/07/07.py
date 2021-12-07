01d498c07dcbded7105c98bbc64c1e0218c90d3d
Jens Christian Hillerup <jens@hillerup.net>
2021-12-07 11:40:37 +0100

    153

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -1,3 +1,2 @@
-def c(n):return n**2+n
-N=sorted(map(int,open('i').read().split(",")))
-print(sum([abs(n-N[len(N)//2]) for n in N]),sum([c(abs(n-sum(N)//len(N))) for n in N])//2)
+N,c,s=sorted(map(int,open('i').read().split(","))),lambda n:n**2+n,sum
+print(s([abs(n-N[len(N)//2])for n in N]),s([c(abs(n-s(N)//len(N)))for n in N])//2)
